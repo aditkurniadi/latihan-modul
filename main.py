97e2baae566cc010634a0557377a1996111bc27e
hauzanarhab123-a11y <hauzanarhab123@gmail.com>
2025-11-06 14:32:58 +0700

    Fitur Programer penambahan fitur print

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-print("Hai Saya Adit Kurniadi")
+# Bagian agung
+import pandas
 
-# fungsi penjumlahan dan pengurangan
-def tambah(a, b):
-    return a + b
-
-def kurang(a, b):
-    return a - b
-
-def kali(a, b):
-    return a * b
-
-print("Hasil Tambah: ", tambah(5, 3))
-print("Hasil Kurang: ", kurang(5, 3))
-print("Hasil Kali: ", kali(5, 3))
-print("halo")
-# Penambahan fitur ojan
+print("Hai")
